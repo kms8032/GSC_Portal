d72821c22a1284e8826eb5dd5b3943c9f6d27cea
KIM 1412 <166794112+kms8032@users.noreply.github.com>
2025-07-09 20:12:23 +0900

    아이디와 비밀번호만으로 DB와 연결되는지 확인함

--- fronted/src/views/register.vue
+++ fronted/src/views/register.vue
@@ -4,7 +4,7 @@
         <form @submit.prevent="handleRegister">
             <input v-model="studentId" placeholder="학번 입력"/>
             <input v-model="password" type="password" placeholder="비밀번호 입력"/>
-            <input v-model="password_check" type="password_check" placeholder="비밀번호 확인"/>
+            <input v-model="password_check" type="password" placeholder="비밀번호 확인"/>
             <button type="submit">회원가입</button>
             <router-link to="/">
                 <button type="button">돌아가기</button>
@@ -15,19 +15,28 @@
 
 <script setup>
 import { ref } from 'vue'
+import axios from 'axios'
 
 const studentId = ref("")
 const password = ref("")
 const password_check = ref("")
 
-function handleRegister() {
+async function handleRegister() {
+    // 회원가입 처리 로직
     if (password.value !== password_check.value) {
         alert("비밀번호가 일치하지 않습니다.")
         return
     }
-    console.log("학번:", studentId.value)
-    console.log("비밀번호:", password.value)
 
-    // 로그인 요청은 추후 axios로 구현 예정
+    try {
+        const response = await axios.post('/api/auth/register', {
+            student_id: studentId.value,
+            password: password.value
+    })
+    alert(response.data.message) // 회원가입 성공 메시지
+    } catch (error) {
+        console.error(error)
+        alert("회원가입 실패: " + (error.response?.data?.message || "서버 오류"))
+    }
 }
 </script>
