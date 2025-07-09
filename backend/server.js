61379a8f297e81fe1c1a7cb833b4c6f8401806d4
KIM 1412 <166794112+kms8032@users.noreply.github.com>
2025-07-09 20:10:17 +0900

    서버실행 코드

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.js'
 
 dotenv.config()
-const app = express()
 
+const app = express()
 app.use(cors())
 app.use(express.json())
 
@@ -13,5 +13,5 @@ app.use('/api/auth', authRoutes)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log("Server running on port ${PORT}")
+    console.log(`Server running on port ${PORT}`)
 })
